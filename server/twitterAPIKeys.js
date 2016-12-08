c9062a016f0196b2057232728480f4486febc210
Emerson Hum <emersonhum@gmail.com>
2016-12-07 21:31:50 -0800

    Removing API key

--- server/twitterAPIKeys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: '1eSlAx4c8c9n4c1RfHdMHg',
-  consumer_secret: 'bFfSW5nEbJlYIWxSEkkqIT6GVsbJUqz15kHKtt4REI',
-  access_token_key: '284331534-555VwAIYNnd19cdXRCfC9vpPwCBwTN7aWpah5w2O',
-  access_token_secret: 'FqjGyk3KsHkJzCFjMcrE1TBKcTeyLOux7QyEuB9mvnIRH'
-};
